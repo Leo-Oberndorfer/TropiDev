a64fd613c275157d8c649b73b70b74851c21e0d0
Leo <l.oberndorfer@students.htl-leonding.ac.at>
2023-01-25 18:02:32 +0100

    Changed the empty course page

--- script/courses/ContentLoader.js
+++ script/courses/ContentLoader.js
@@ -6,34 +6,50 @@ let properties = ["course-heading", "course-description", "course-intro", "cours
 
 function setHomeAppearance(){
     fetch(file)
-        .then((response) => {return response.json();})
-        .then((data) => {
-            for(let property of properties) {
-                document.getElementById(property).innerHTML = data[property];
+        .then(response => {
+            if(response.ok){
+                response.json().then(data => {
+                    document.title = `Developer Island | ${selectedCourseUpper} Course`;
+                    for (let property of properties) {
+                        document.getElementById(property).innerHTML = data[property];
+                    }
+
+                    let difficulty = document.getElementById("course-difficulty");
+                    difficulty.setAttribute("class", `size-16 level-${difficulty.innerHTML.toLowerCase()}`);
+
+                    let list = document.getElementById("course-skills");
+                    for (let item of data["course-skills"]) {
+                        let li = document.createElement("li");
+                        li.innerHTML = item;
+                        list.appendChild(li);
+                    }
+
+                    calculateUserProgress(data);
+                    setStyle(data);
+
+                    //Button
+                    document.getElementById("course-start-btn").innerHTML = "Start learning"; //Calculate this
+                });
+            } else {
+                courseNotFound();
             }
+        })
+}
 
-            let difficulty = document.getElementById("course-difficulty");
-            difficulty.setAttribute("class", `size-16 level-${difficulty.innerHTML.toLowerCase()}`);
+function courseNotFound(){
+    document.getElementById("course-body").innerHTML =
+        "<h1 style='font-size: 64px; font-weight: 750; color: #230d34;'>Course not found</h1>" +
+        "<p style='font-size: 18px; font-weight: 500; color: #00172b'>The course you were looking for does not exist (yet)</p>";
+    document.getElementById("course-body").style.cssText = "text-align: center; padding: 230px 25px 230px 25px;";
+    console.log("File not found, enter a valid course.");
+}
 
-            let list = document.getElementById("course-skills");
-            for(let item of data["course-skills"]){
-                let li = document.createElement("li");
-                li.innerHTML = item;
-                list.appendChild(li);
-            }
-
-            calculateUserProgress(data);
-
-            //Style
-            document.getElementById("course-image").src = `../../assets/courses/${selectedCourseLower}/${selectedCourseLower}.png`;
-            document.getElementById("course-start").style.backgroundColor = data["bg-color"];
-            let listStyle = document.body.appendChild(document.createElement("style"));
-            listStyle.innerHTML = `#course-skills li::before {content: "\\2022"; color: ${data["list-item-dot-color"]}; display: inline-block; font-weight: bold; margin-left: -20px; width: 20px;}`;
-
-            //Button
-            document.getElementById("course-start-btn").innerHTML = "Start learning"; //Calculate this
-            document.getElementById("course-start-btn").style.cssText = `margin-top: 25px; background-color: ${data["btn-color"]}; border: none; font-size: 18px;`;
-        });
+function setStyle(data){
+    document.getElementById("course-image").src = `../../assets/courses/${selectedCourseLower}/${selectedCourseLower}.png`;
+    document.getElementById("course-start").style.backgroundColor = data["bg-color"];
+    let listStyle = document.body.appendChild(document.createElement("style"));
+    listStyle.innerHTML = `#course-skills li::before {content: "\\2022"; color: ${data["list-item-dot-color"]}; display: inline-block; font-weight: bold; margin-left: -20px; width: 20px;}`;
+    document.getElementById("course-start-btn").style.cssText = `margin-top: 25px; background-color: ${data["btn-color"]}; border: none; font-size: 18px;`;
 }
 
 function calculateUserProgress(data){
