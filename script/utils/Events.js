336400a03541ae83880fae574b5e67eca45e92cb
Leo <l.oberndorfer@students.htl-leonding.ac.at>
2023-03-01 22:18:11 +0100

    Added fading and sliding animations to the containers from the course site

--- script/utils/Events.js
+++ script/utils/Events.js
@@ -1,5 +1,36 @@
 
 function registerCourseEvents() {
-    document.getElementById("course-start-btn").addEventListener('click', () => {
+    getElement("course-start-btn").addEventListener('click', () => {
+        let introContainer = getElement("course-intro-container");
+        let courseContainer = getElement("course-container");
+        let infoList = getElement("course-info-list");
+        let progressBar = getElement("course-progress-bar");
+        let courseOverviewContainer = getElement("course-overview-container");
+
+        introContainer.classList.add("fade-out", "slide-out");
+        infoList.classList.add("fade-out");
+        courseContainer.classList.remove("hidden");
+        courseContainer.classList.add("fade-in");
+
+        moveUserProgress(infoList.offsetHeight, progressBar);
+
+        let offsetY = introContainer.offsetHeight;
+        courseContainer.style.transform = `translateY(-${offsetY}px)`;
+
+        courseContainer.addEventListener("transitionend", () => {
+            introContainer.remove();
+            infoList.remove();
+            courseOverviewContainer.appendChild(progressBar);
+            progressBar.style.position = "static";
+            progressBar.style.transform = "translateY(0px)";
+            courseContainer.style.transition = "none";
+            courseContainer.style.transform = "translateY(0px)";
+        });
     });
+}
+
+function moveUserProgress(moveTo, progressBarElement){
+    progressBarElement.style.width = "90%";
+    progressBarElement.style.position = "absolute";
+    progressBarElement.style.transform = `translateY(-${moveTo}px)`;
 }
