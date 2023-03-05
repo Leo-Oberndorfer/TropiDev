f9c31820a2cc13ab61ac34a25d6aec96b345e1c8
LisaWieshofer <l.wieshofer@students.htl-leonding.ac.at>
2023-03-05 15:34:40 +0100

    made the carousel work

--- script/home/HomeScript.js
+++ script/home/HomeScript.js
@@ -1,28 +1,30 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centeredSlides: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
+function loadSwiper() {
+    var swiper = new Swiper(".slide-content", {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        loop: true,
+        centeredSlides: 'true',
+        fade: 'true',
+        grabCursor: 'true',
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
         },
-        520: {
-            slidesPerView: 2,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
-        950: {
-            slidesPerView: 3,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            760: {
+                slidesPerView: 2,
+            },
+            950: {
+                slidesPerView: 3,
+            },
         },
-    },
-});
+    });
+}
