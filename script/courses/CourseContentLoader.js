00b0a3b8806ed7b95714b73c0b61f79c19e5486a
Leo <l.oberndorfer@students.htl-leonding.ac.at>
2023-03-05 22:06:12 +0100

    Added some content to the first chapter under the html course

--- script/courses/CourseContentLoader.js
+++ script/courses/CourseContentLoader.js
@@ -5,21 +5,42 @@ function loadChapters(chapters){
     for (let chapter in chapters) {
         chaptersList.innerHTML +=
             "<div class=\"accordion-item\">" +
-            "   <h2 class=\"accordion-header\" id=\"flush-heading" + idx + "\">" +
-            "       <button class=\"accordion-button collapsed\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#flush-collapse" + idx + "\" aria-expanded=\"false\" aria-controls=\"flush-collapse" + idx + "\">" +
-            "           <div class=\"task-circle\" id=\"task-circle-\"" + idx + "></div>" +
+            "   <h2 class=\"accordion-header\" id=\"flush-heading-" + idx + "\">" +
+            "       <button class=\"accordion-button collapsed\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#flush-collapse" + idx + "\" aria-expanded=\"false\" aria-controls=\"flush-collapse-" + idx + "\">" +
+            "           <div class=\"task-circle\" id=\"task-circle-" + idx + "\"></div>" +
             "           <b>" +
             chapters[chapter]["chapter-title"] +
             "           </b>" +
             "       </button>" +
             "   </h2>" +
-            "   <div id=\"flush-collapse" + idx + "\" class=\"accordion-collapse collapse\" aria-labelledby=\"flush-heading" + idx + "\" data-bs-parent=\"#course-chapter-list\">" +
-            "       <div class=\"accordion-body\">" +
-            chapters[chapter]["chapter-task-description"] +
+            "   <div id=\"flush-collapse" + idx + "\" class=\"accordion-collapse collapse\" aria-labelledby=\"flush-heading-" + idx + "\" data-bs-parent=\"#course-chapter-list\">" +
+            "       <div class=\"accordion-body\" id=\"chapter-body-" + idx + "\">" +
+            "       <p>" + chapters[chapter]["chapter-description"] + "</p>" +
             "       </div>" +
             "   </div>" +
             "</div>"
         ;
+        loadChapterBody(chapters, chapter, idx);
         idx++;
+    }
+}
+
+function loadChapterBody(chapters, chapter, idx){
+    let chapterBody = getElement("chapter-body-" + idx);
+    let i = 0;
+    while(i < chapters[chapter]["chapter-paragraph-headings"].length){
+        let heading = chapters[chapter]["chapter-paragraph-headings"][i];
+        let paragraph = chapters[chapter]["chapter-paragraphs"][i];
+        let img = chapters[chapter]["chapter-paragraph-images"][i];
+
+        if(img !== ""){
+            let float = i % 2 !== 0 ? "left" : "right";
+            img = "<img src=\"" + img + "\" alt=\"course-asset\" style=\"height: 115px; float: "+ float +";\">";
+        }
+        chapterBody.innerHTML +=
+            "<h4>" + heading + "</h4>" +
+            "<p>" + img + paragraph + "</p>"
+        ;
+        i++;
     }
 }
