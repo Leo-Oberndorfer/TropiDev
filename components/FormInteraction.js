710c1ba7ed5b787f77210168667bd43d648c0607
Leo <l.oberndorfer@students.htl-leonding.ac.at>
2023-08-22 21:27:20 +0200

    Fixed scroll bug + coco carousel prototyp v2

--- components/FormInteraction.js
+++ components/FormInteraction.js
@@ -1,11 +1,16 @@
 function InputRegisterEmail(){
-    showForm();
+    showRegisterForm();
     const email = document.getElementById("register-prompt-email");
     const registerFormEmail = document.getElementById("register-form-email");
     registerFormEmail.value = email.value;
 }
 
-function showForm(){
+function showRegisterForm(){
     const overlay = document.getElementById("register-overlay");
     overlay.classList.remove("hidden");
+}
+
+function hideRegisterForm(){
+    const overlay = document.getElementById("register-overlay");
+    overlay.classList.add("hidden");
 }
